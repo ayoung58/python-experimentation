4c3fdf184d0ea5f4b239a2dc4bb08afdc4f01607
Theinterna <yalvin58@gmail.com>
2024-06-04 14:22:15 -0700

    Add escape character and multiline features

---
+++ python-basics-string-manipulation.py
@@ -0,0 +1,8 @@
+# Can use \ as escape character
+# if you add r in front of quotes, it becomes a raw string (so backslashes would also be printed)
+
+print(r"C:\yes\ma'am")
+# If you want a string message to be multiline, you can use triple quotes '''
+'''multiline string is 
+used for comments that span
+multiple lines :)'''

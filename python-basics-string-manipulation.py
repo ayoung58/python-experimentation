921a5bf409960bb0cc020d3d1b265539132996f6
Theinterna <yalvin58@gmail.com>
2024-06-04 14:23:09 -0700

    Move string manipulation from no func. to str man.

--- python-basics-string-manipulation.py
+++ python-basics-string-manipulation.py
@@ -6,3 +6,17 @@ print(r"C:\yes\ma'am")
 '''multiline string is 
 used for comments that span
 multiple lines :)'''
+
+# String manipulation:
+bob = "Mynameisbob"
+# Start, end (not included), step size
+print(bob[2:5:1])
+# return every other
+print(bob[::2])
+
+# To trim, use .strip(). By default, removes whitespace, but if you add characters inside parentheses, will remove those
+# lstrip and rstrip removes from only the left and right, respectively.
+
+testWord = "yyxxyyHelloyyxxyy"
+testWordEdited = testWord.strip("xy") # Outputs Hello
+print(testWordEdited)
